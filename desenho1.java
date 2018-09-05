6494fd35153b75728a69a36cff5a9716b17961cd
miihsoraya <38866795+miihsoraya@users.noreply.github.com>
2018-09-05 15:00:18 -0300

    Create desenho1.java

---
+++ desenho1.java
@@ -0,0 +1,29 @@
+package desenho;
+
+/*
+
+**********
+*********
+********
+*******
+******
+*****
+****
+***
+**
+*
+
+ */
+public class Desenho {
+
+    public static void main(String[] args) {
+
+        for(int i=0;i<10;i++){
+            for(int j=0;j<10-i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        } 
+    }
+    
+}
