fc6cb7073aa05338393aeef81c14877a2e13b1be
miihsoraya <38866795+miihsoraya@users.noreply.github.com>
2018-09-05 14:38:03 -0300

    Update TesteQuestao.java

--- TesteQuestao.java
+++ TesteQuestao.java
@@ -1,7 +1,8 @@
+package testequestao;
 
-package questaojav;
+import java.util.Scanner;
 
-public class QuestaoJav {
+public class TesteQuestao {
 
     /*Um palíndromo é uma seqüência de caracteres que é lida da esquerda para a direita
 ou da direita para a esquerda da mesma forma. Por exemplo, cada um dos seguintes
@@ -11,27 +12,31 @@ Se o número não for de cinco dígitos, o programa deve rejeitá-lo e pedir out
 número. (Nota: não utilize vetores ou strings para resolver este problema)
      */
     public static void main(String[] args) {
-           String n="2321";
-           int falso = 0, verdadeiro = 0;
-           int meio = n.length() / 2;
-           
-           if(n.length()==5){
-                for (int i = 0; i < meio; i++) {
-                    if (n.charAt(i) != n.charAt(n.length()-1 - i)) {
-                        falso++;
-                    }else{
-                        verdadeiro++;
+           Scanner pali = new Scanner(System.in);
+           int falso = 0, verdadeiro = 0, s =0;
+                      
+           while(s<5){ // repeticao de 5 vezes
+               System.out.println("Digite um numero com 5 digitos:   ");
+               String n = pali.next();
+               int meio = n.length() / 2; // comprimento divido por dois. localizando a metade da string
+               if(n.length()==5){
+                    for (int i = 0; i < meio; i++) { //meio=2 ... 0...1
+                        if (n.charAt(i) != n.charAt(n.length()-1 - i)) { //localizacao(0 e 1) diferente do comprimento(3 e 4)
+                            falso++;
+                        }
                     }
-                }    
-           }else{
-               System.out.println("Escreva um numero com 5 digitos");
+                    if(falso>0){ //se for encotrado uma ou mais numeros diferentes é considerado falso
+                        System.out.println("Nao sao iguais");
+                    }else{
+                        System.out.println("Sao iguais");
+                    }    
+               }else{
+                    System.out.println("Escreva um numero com 5 digitos.   \n\n");
+               }
+               falso = 0;
+               s++;
            }
-           
-           if(falso>0){
-               System.out.println("Nao sao iguais");
-           }else if(verdadeiro>1){
-               System.out.println("Sao iguais");
-           }
+
 
            
     }
